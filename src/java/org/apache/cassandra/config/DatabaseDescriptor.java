cc9e203fd46ac471cbf51e43a4f4be94c33c4f81
David Capwell <dcapwell@apache.org>
2024-10-28 09:54:05 -0700

    Get Harry working on top of Accord and fix various issues found by TopologyMixupTestBase
    patch by David Capwell; reviewed by Alex Petrov, David Capwell for CASSANDRA-20054

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -3662,7 +3662,7 @@ public class DatabaseDescriptor
 
     public static AccordSpec getAccord()
     {
-        return conf.accord;
+        return conf == null ? null : conf.accord;
     }
 
     public static AccordSpec.TransactionalRangeMigration getTransactionalRangeMigration()
@@ -5731,9 +5731,14 @@ public class DatabaseDescriptor
         conf.cms_default_max_retries = value;
     }
 
-    public static DurationSpec getDefaultRetryBackoff()
+    public static DurationSpec.IntMillisecondsBound getDefaultRetryBackoff()
     {
         return conf.cms_default_retry_backoff;
+    }
+
+    public static DurationSpec.IntMillisecondsBound getDefaultMaxRetryBackoff()
+    {
+        return conf.cms_default_max_retry_backoff;
     }
 
     public static DurationSpec getCmsAwaitTimeout()
